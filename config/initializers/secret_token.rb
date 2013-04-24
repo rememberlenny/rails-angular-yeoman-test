f2145b8e7fd065851129918f61b131eb285282b9
lkbgift <lkbgift@gmail.com>
2013-04-24 16:24:39 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LentRb::Application.config.secret_key_base = '54b00a4ee10e532735d65d6cc46bd87523777eae4742d41a06f531d7dfb90bd11d5fb40dce0509ccf422cd26f669d8211b362a5e110f402647fd3212859db055'
